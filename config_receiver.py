507f6665fea1002d384508d766acfb8f3ffa6f2f
h-jamil <mdhasibul.jamil@siu.edu>
2023-07-03 17:04:42 -0400

    Added sender and receiver along with their config and file creation files

---
+++ config_receiver.py
@@ -0,0 +1,10 @@
+configurations = {
+    "receiver": {
+        "host": "164.54.113.195",
+        "port": 8000
+    },
+    "data_dir": "/home/beams/MJAMIL/data/dest",
+    "max_cc": 100,
+    "file_transfer": True,
+    "loglevel": "info",
+}
---
+++ file_creation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if the correct number of arguments are passed
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <destination> <num_files> <file_size>"
+    exit 1
+fi
+
+# Assign variables from arguments
+dest_dir="$1"
+num_files="$2"
+file_size="$3"
+
+# Create directory if it doesn't exist
+if [ ! -d "$dest_dir" ]; then
+    mkdir "$dest_dir"
+fi
+
+# Create num_files of file_size bytes
+for ((i=1; i<=num_files; i++))
+do
+    dd if=/dev/zero of="$dest_dir"/file"$i".txt bs=1 count="$file_size" >/dev/null 2>&1
+done
+
+echo "Created $num_files files of size $file_size bytes in $dest_dir"
